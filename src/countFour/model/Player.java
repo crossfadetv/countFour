68bd6d5649d4079480c59dc9a5b6f2385e2e53ba
Krubally <rahel.krubally@accenture.com>
2020-01-18 20:20:34 +0100

    add comments to Player class

--- src/countFour/model/Player.java
+++ src/countFour/model/Player.java
@@ -10,7 +10,12 @@ public class Player {
     private ArrayList<Stone> stones;
     private boolean turn;
 
-    public Player(String playerName, Color color, boolean turn){
+    /***
+     * @param color color that will be set for the player
+     * @param playerName name that will be set for the player
+     * @param turn whether or not the player is on turn
+     */
+     public Player(String playerName, Color color, boolean turn){
         stones = new ArrayList<>();
         this.name = playerName;
         this.color = color;
@@ -18,40 +23,65 @@ public class Player {
         fillStones();
     }
 
+    /**
+     * @return the players name
+     * */
     public String getName(){
         return name;
     }
 
+    /**
+     * @param name name can be set for player
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * @return the color of the player
+     * */
     public Color getColor() {
         return color;
     }
 
+    /**
+     * @return whether or not it's the players turn
+     * */
     public boolean onTurn(){
         return turn;
     }
 
+    /**
+     * @return the players stones
+     * */
     public ArrayList getStones() {
         return stones;
     }
+
+    /**
+     * @return the stone the player throws
+     * */
     public Stone playStone(){
         Stone stone = stones.get(stones.size()-1);
         stones.remove(stones.size()-1);
         return stone;
     }
 
+    /**
+     * @return the amount of stones the player has
+     * */
     public int countStones(){
        return stones.size();
     }
 
+    /**fills the players stones*/
     public void fillStones(){
         for(int x=0; x<21;x++){
             stones.add(new Stone(this));
         }
     }
+
+    /** changes whether or not it's the player's turn*/
     public void changeTurn(){
             this.turn = !this.turn;
         }
